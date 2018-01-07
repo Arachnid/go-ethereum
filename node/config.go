32a30ee43f260d2fb9ca9066a1642dcac727b0c6
Nick Johnson <arachnid@notdot.net>
2018-01-07 18:38:11 +0000

    accounts, core, internal, node: Add support for smartcard wallets

--- node/config.go
+++ node/config.go
@@ -27,6 +27,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/accounts/scwallet"
 	"github.com/ethereum/go-ethereum/accounts/usbwallet"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -431,6 +432,12 @@ func makeAccountManager(conf *Config) (*accounts.Manager, string, error) {
 			log.Warn(fmt.Sprintf("Failed to start Trezor hub, disabling: %v", err))
 		} else {
 			backends = append(backends, trezorhub)
+		}
+		// Start a smart card hub
+		if schub, err := scwallet.NewHub(scwallet.Scheme, keydir); err != nil {
+			log.Warn(fmt.Sprintf("Failed to start smart card hub, disabling: %v", err))
+		} else {
+			backends = append(backends, schub)
 		}
 	}
 	return accounts.NewManager(backends...), ephemeral, nil
